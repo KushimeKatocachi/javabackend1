fe8b9b7aef36ee559f8b87d023923719c3697d3a
User <tashenov05@gmail.com>
2022-12-01 13:02:50 +0600

    Initial commit

---
+++ src/heyyy.java
@@ -0,0 +1,22 @@
+public class heyyy {
+    public static void main(String[] args) {
+        int a = 6 + 7;
+        int b = 1;
+
+
+        b++;
+        System.out.println(b);
+
+        b += 5;
+        b = b + 5;
+
+        b /= 5;
+        b = b / 5;
+
+        b -= 5;
+        b = b - 5;
+
+        b *= 5;
+        b = b * 5;
+    }
+}
